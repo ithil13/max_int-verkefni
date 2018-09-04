65dc5b9d5b5c1afa5aa70459b2418f447cdddc0a
ithil13 <ithil13@gmail.com>
2018-09-04 11:43:20 +0000

    fixed comments

--- sequence.py
+++ sequence.py
@@ -1,7 +1,7 @@
 # skref 1 taka við n frá notanda
 # skref 2 búa til lista með fyrstu 3 tölunum
 # skref 3 leggja saman síðustu 3 tölur og bæta við listann
-# skref 4 endurtaka skref 3 þar það eru komnar n tölur í listann
+# skref 4 endurtaka skref 3 þar til það eru komnar n tölur í listann
 # skref 5 prenta út listann
 
 n = int(input("Enter the length of the sequence: "))
