00e05473d9d97eadf263cd15e562321049b9356b
ithil13 <ithil13@gmail.com>
2018-09-04 12:08:42 +0000

    fixed code in sequence.py

--- sequence.py
+++ sequence.py
@@ -2,7 +2,7 @@
 # skref 2 búa til lista með fyrstu 3 tölunum
 # skref 3 leggja saman síðustu 3 tölur og bæta við listann
 # skref 4 endurtaka skref 3 þar til það eru komnar n tölur í listann
-# skref 5 prenta út listann
+# skref 5 prenta út rétt magn af tölum
 
 n = int(input("Enter the length of the sequence: "))
 
@@ -12,4 +12,10 @@ while len(sequence) < n:
     temp_num = sequence[-1] + sequence[-2] + sequence[-3]
     sequence.append(temp_num)
 
-print(*sequence, sep = "\n")
+if n == 1:
+    print("1")
+elif n == 2:
+    print("1")
+    print("2")
+else:
+    print(*sequence, sep = "\n")
