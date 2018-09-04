bf88cd821ee3f81049ac106b4672eaa4ef37d530
ithil13 <ithil13@gmail.com>
2018-09-04 10:11:35 +0000

    added code to max_int.py

--- max_int.py
+++ max_int.py
@@ -1,8 +1,18 @@
 # Design an algorithm that finds the maximum positive integer input by a user.  
 # The user repeatedly inputs numbers until a negative value is entered.
 
-# skref 1 búa til breytu fyrir max_int
-# skref 2 taka við number frá notanda
-# skref 3 bera saman max_int og number, ef number er hærri breytist gildi max_int
+# skref 1 búa til breytur fyrir max_int og num_int
+# skref 2 taka við num_int frá notanda
+# skref 3 bera saman max_int og num_int, ef number er hærri breytist gildi max_int
 # skref 4 endurtaka 2 og 3 þangað til sett er inn neikvæð tala
 # skref 5 prenta max_int
+
+max_int = 0
+num_int = 0
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+
+print("The maximum is", max_int)
